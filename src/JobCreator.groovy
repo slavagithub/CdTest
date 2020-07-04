af186687997ee3f0370d2853062f4fee81fee55e
Viacheslav Sytnyk <VyacheslavS@mymiratech.com>
2020-07-04 15:37:18 +0300

    space

--- src/JobCreator.groovy
+++ src/JobCreator.groovy
@@ -8,9 +8,9 @@ import okhttp3.Response
 
 class JobCreator{
 
-    static String getNewJobXml(String dir, String subDir, String name, String lob, String email){
+    static String getNewJobXml(String subDir, String name, String lob, String email){
 
-        String content = getJobXml(dir, subDir, name)
+        String content = getJobXml(subDir, name)
         return replaceFields(content, lob, email)
 
     }
@@ -21,16 +21,13 @@ class JobCreator{
         return contentWithEmailSet
     }
 
-    static String getJobXml(String dir, String subDir, String name){
-        dir = dir.replaceAll("\u0020", "%20")
-        subDir = subDir.replaceAll("\u0020", "%20")
-
+    static String getJobXml(String subDir, String name){
         def username = "Slava"
         def password = "11e110cc052863bb70d5775e14888c2718"
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url("http://localhost:8080/job/${dir}/job/${subDir}/job/${name}/config.xml")
+                .url("http://localhost:8080/job/Routing%20Engeniering/job/${subDir}/job/${name}/config.xml")
                 .method("GET", null)
                 .addHeader("Cookie", "JSESSIONID.b1305f6e=node016bienql94kr62spbibbh9v7p5.node0")
                 .addHeader("Authorization",  "Basic ${"$username:$password".bytes.encodeBase64()}")
@@ -39,17 +36,16 @@ class JobCreator{
 
         return response.body().string()
     }
-    static String addJob(String folderName, String subFolderName, String name, String lob, String email){
+    static String addJob(String subFolderName, String name, String lob, String email){
         def jenkins = Jenkins.instance
 
-        String content = getNewJobXml(folderName, subFolderName, name, lob, email)
+        String content = getNewJobXml(subFolderName, name, lob, email)
 
         def xmlStream = new ByteArrayInputStream(content.getBytes() )
 
-        def folder = jenkins.getItem(folderName)
-        def subFolder = folder.getItem(subFolderName)
+        def subFolder = jenkins.getItem(subFolderName)
 
-        def newName = name.replaceAll("New", "Existing")
+        def newName = name.replaceAll("Inside", "Outside")
 
         subFolder.createProjectFromXML(newName, xmlStream)
         return newName+ " job successfully created"
